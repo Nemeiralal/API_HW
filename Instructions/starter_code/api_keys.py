9630637a53d9005d08e99828147e2e289ec0eaeb
Nemeiralal <46546311+Nemeiralal@users.noreply.github.com>
2019-03-07 22:07:30 -0600

    HW done

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,26 +1,3 @@
 
-#%%
-# Dependencies
-import json
-import requests
-from api_keys import api_key
-
-
-#%%
-# Save config information
-url = "http://api.openweathermap.org/data/2.5/weather?"
-city = "London"
-
-# Build query URL
-query_url = url + "appid=" + api_key + "&q=" + city
-
-
-#%%
-# Get weather data
-weather_response = requests.get(query_url)
-weather_json = weather_response.json()
-
-# Get the temperature from the response
-print(f"The weather API responded with: {weather_json}.")
-
+api_key = "c2747a1a47120dfe77da0d1c5bfd583b"
 
